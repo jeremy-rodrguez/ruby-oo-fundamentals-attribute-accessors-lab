937dcfcd66ba24f8dcfb54fb32de397a66bc4271
Jeremy <jeremy.rodrguez@yahoo.com>
2020-11-17 23:32:09 -0800

    11:26pm Epiphany

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,17 @@
-## code your solution here. 
+## code your solution here.
+# The attr_ creates "name=" and "name" as a shortcut if you were to invoke them in the future. 
+class Cat
+    attr_accessor :name
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def meow
+        puts "meow!"
+    end
+end
